29fa621ebafa0519979570fb568cbe637dd403cd
Kristian Glendrange <kristian.glendrange@visma.com>
2023-04-05 16:28:19 +0200

    Adding useAbility (this is really just a git test)

--- src/Game.ts
+++ src/Game.ts
@@ -1,6 +1,6 @@
 // Game.ts
-import type { Game, Move } from "boardgame.io";
-import { Card, Player } from "./types";
+import type { Ctx, Game, Move } from "boardgame.io";
+import { Ability, Camp, Card, Person, Player } from "./types";
 import { sniper } from "./cards";
 
 // The game state passed to any handlers
@@ -10,13 +10,13 @@ export interface GameState {
   discardPile: Card[];
 }
 
-function setupPlayer(): Player {
+function setupPlayer(first?: boolean): Player {
   return {
     hand: [],
     people: [[], [], []],
     camps: [],
     eventQueue: [],
-    remainingWater: 3,
+    remainingWater: first ? 1 : 3,
     isRaidersAvailable: true,
     isWaterSiloAvailable: true,
   };
@@ -29,7 +29,7 @@ function setupDeck(): Card[] {
 export const Radlands: Game<GameState> = {
   setup: function () {
     return {
-      players: [setupPlayer(), setupPlayer()],
+      players: [setupPlayer(true), setupPlayer()],
       deck: setupDeck(),
       discardPile: [],
     };
@@ -48,5 +48,39 @@ export const Radlands: Game<GameState> = {
     reShuffle: ({ G, playerId, ctx }, id) => {
       G.deck = setupDeck();
     },
+    useAbility: (
+      { G, ctx, playerID },
+      attacker: Person | Camp,
+      ability: Ability,
+      target: string
+    ) => {
+      //check that they have the cost
+      const player = getPlayer(G, ctx);
+      handlePayment(player, ability.waterCost);
+      //how is the target selected?
+      //is it a valid target?
+
+      //complete the effect
+
+      //after effects
+      attacker.isReady = false;
+    },
   },
 };
+
+function getPlayer(G: GameState, ctx: Ctx) {
+  return G.players[ctx.playOrderPos];
+}
+
+function getOpponent(G: GameState, ctx: Ctx) {
+  return G.players[ctx.playOrderPos + 1];
+}
+
+function handlePayment(player: Player, cost: number): boolean {
+  if (player.remainingWater < cost) {
+    return false;
+  } else {
+    player.remainingWater -= cost;
+    return true;
+  }
+}
