d8ae6923681c4693c427d4e30366bf7070b1a881
Simon Dein <simon.dein@visma.com>
2023-04-05 17:50:59 +0200

    fleshing out board rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Client } from "boardgame.io/react";
-import { Board } from "./Board";
+import { Board } from "./components/Board";
 import { Radlands } from "./Game";
 
 export default Client({ game: Radlands, board: Board });
--- src/components/Deck.tsx
+++ src/components/Deck.tsx
@@ -1,3 +1,9 @@
-export function Deck() {
-  return <div>Deck</div>;
+import { Card } from "../types";
+
+export function Deck({ deck }: { deck: Card[] }) {
+  return (
+    <div className="border border-solid w-full">
+      <span>{deck.length}</span> cards in deck
+    </div>
+  );
 }
